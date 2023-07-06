2c8c916ae80c2b5783ec6ce6b6c5d5e594c640b9
G285122022 <228512@st.takushoku-u.ac.jp>
2023-07-06 18:14:51 +0900

    07_06

--- kazuate.js
+++ kazuate.js
@@ -4,42 +4,56 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let a = document.querySelector('button#print');
+a.addEventListener('click',hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-   
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-    let yoso = 4;
-      kaisu = kaisu+1;
-    if(kaisu===4){
-        console.log("答えは ◯ でした．すでにゲームは終わっています");
-    }else if (kaisu===3){
-        if(yoso===kotae){
-            console.log("正解です．おめでとう!");
-         }else  {
-            console.log("まちがい．残念でした答えは ◯ です")
-         }
-    }else{
-        if(yoso===kotae){
-            console.log("正解です．おめでとう!");
-         }else if(yoso>kotae){
-             console.log("まちがい．答えはもっと小さいですよ");
-         }else if (yoso<kotae){
-            console.log("まちがい．答えはもっと大きいですよ");
-         }
-    }
-   
-    
+   let q = document.querySelector('input[name="answer"]');
+   let number = q.value;
+   console.log(number);
+  
+  kaisu = kaisu+1;
+  let k1 = document.querySelector('span#kaisu');
+  k1.textContent = kaisu;
+
+  let yoso  = Number(q.value);
+  let yoso1 = document.querySelector('span#answer');
+  yoso1.textContent=yoso;
+  
+  
+  let e3 = document.querySelector('p#result');
+
+  if(kaisu===1){
+      if(kotae===yoso){
+          e3.textContent = '正解です.おめでとう!';
+      }else if(kotae>yoso){
+          e3.textContent = 'まちがい.答えはもっと大きいですよ';
+      }else if(kotae<yoso){
+          e3.textContent = 'まちがい.答えはもっと小さいですよ';
+      }
+  }if(kaisu===2){
+      if(kotae===yoso){
+          e3.textContent = '正解です.おめでとう!';
+      }else if(kotae>yoso){
+          e3.textContent = 'まちがい.答えはもっと大きいですよ';
+      }else if(kotae<yoso){
+          e3.textContent = 'まちがい.答えはもっと小さいですよ';
+      }
+  }if(kaisu===3){
+      if(kotae===yoso){
+          e3.textContent = '正解です.おめでとう!';
+      }else{
+          e3.textContent = 'まちがい.残念でした答えは'+kotae+'です.';
+      }
+  }if(kaisu>3){
+      e3.textContent = '答えは '+kotae+' でした. すでにゲームは終わっています';
+  }
+}
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-}
